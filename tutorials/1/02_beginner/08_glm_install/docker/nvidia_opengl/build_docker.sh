40490457352437ca53297a8f04f971e699c50648
Andrei - Doru Pata <pandreidoru@gmail.com>
2020-12-05 14:13:47 +0000

    T1/02/08 - Install GLM

---
+++ tutorials/1/02_beginner/08_glm_install/docker/nvidia_opengl/build_docker.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -e
+
+function usage() {
+    echo "usage: $0 [-i <IMAGE_NAME> [-t <TAG>]] | [-h]"
+    echo "-i | --image  Docker image name"
+    echo "-t | --tag    Docker tag (default $TAG)"
+    echo "-h | --help   This message"
+}
+
+IMAGE_NAME="nvidia/opengl"
+IMAGE_TAG="1.2-glvnd-devel-ubuntu20.04"
+
+# Iterate through command line inputs.
+while [ "$1" != "" ]; do
+    case $1 in
+    -i | --image)
+        shift
+        IMAGE_NAME=$1
+        ;;
+    -t | --tag)
+        shift
+        IMAGE_TAG=$1
+        ;;
+    -h | --help)
+        usage
+        exit
+        ;;
+    *)
+        usage
+        exit 1
+        ;;
+    esac
+    shift
+done
+
+# Use new features.
+export DOCKER_BUILDKIT=1
+
+docker build --progress=plain -t pandreidoru/"${IMAGE_NAME}":"${IMAGE_TAG}" .
